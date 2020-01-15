ddafd94d9b888f35c0fed63c85824332790bf8bc
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2020-01-15 21:23:52 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,18 @@
 // Code SimpleComponent Here
+    class ComponentName extends Component {
+      constructor(props) {
+        super(props);
+     
+        // Initial state here...
+        this.state = {
+        };
+      }
+     
+      handleClick = () => {
+        // Probably do some work to update the state
+      }
+     
+      render() {
+        // Return JSX that renders into HTML
+      }
+    }
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,19 @@
 // Code SimplerComponent Here
+
+    class ComponentName extends Component {
+      constructor(props) {
+        super(props);
+     
+        // Initial state here...
+        this.state = {
+        };
+      }
+     
+      handleClick = () => {
+        // Probably do some work to update the state
+      }
+     
+      render() {
+        // Return JSX that renders into HTML
+      }
+    }
